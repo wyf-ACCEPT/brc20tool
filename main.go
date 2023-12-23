d5bb2e5990afe9d6e342ed8a80c6b43701383779
wyf-ACCEPT <define_wifi@163.com>
2023-12-23 20:43:54 +0800

    add args

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/minchenzz/brc20tool/internal/ord"
 	"github.com/minchenzz/brc20tool/pkg/btcapi/mempool"
@@ -21,8 +20,9 @@ import (
 var privateKey string
 
 func main() {
-	fmt.Println("============ brc20 tool ============")
+	fmt.Println("======================= brc20 tool =======================")
 
+	// ------------------- Load the private key -------------------
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
@@ -35,29 +35,62 @@ func main() {
 		return
 	}
 
-	forEstimate := true
-	brc20op := "mint"
-	brc20tick := "omni"
-	brc20amt := "100"
-	brc20repeat := "2"
-	feePerBytes := "2"
+	// ------------------- Parse the brc20 params -------------------
+	forEstimate := flag.Bool("simulate", false, "Whether to simulate the transaction (true/false). Default is false.")
+	brc20op := flag.String("op", "", "BRC20 operation (e.g., `mint`, `transfer` or `deploy`).")
+	brc20tick := flag.String("tick", "", "BRC20 ticker symbol (e.g., `ordi`).")
+	brc20amt := flag.String("amt", "", "BRC20 amount (e.g., 100).")
+	brc20repeat := flag.Int("repeat", 1, "Number of times to repeat the operation. Default is 1.")
+	feePerBytes := flag.Int("fee", 10, "Transaction fee per byte. Default is 10.")
+	showHelp := flag.Bool("help", false, "Show help.")
+	flag.Parse()
 
-	txid, txids, fee, err := constructBRC20(forEstimate, brc20op, brc20tick, brc20amt, brc20repeat, feePerBytes)
+	if *showHelp {
+		flag.Usage()
+		return
+	}
+
+	shouldReturn := checkParamsForBRC20(brc20op, brc20tick, brc20amt)
+	if shouldReturn {
+		return
+	}
+
+	// ------------------- Construct the transaction -------------------
+	fmt.Println("\n-------------- construct the transaction --------------")
+	txid, txids, fee, err := constructBRC20(*forEstimate, *brc20op, *brc20tick, *brc20amt, *brc20repeat, *feePerBytes)
 
 	if err != nil {
 		fmt.Println("Error in running:", err)
 		return
 	}
 
-	fmt.Println("\n[INFO] transactions (if not simulating):")
-	fmt.Println("prepare utxo txid:", txid)
-	fmt.Println("inscription txids:", txids)
-	fmt.Println("fee:", fee)
+	if !*forEstimate {
+		fmt.Println("\n------------------ transactions info ------------------")
+		fmt.Println("prepare utxo txid:", txid)
+		fmt.Println("inscription txids:", txids)
+		fmt.Println("fee:", fee)
+	}
 }
 
-func constructBRC20(forEstimate bool, brc20op string, brc20tick string, brc20amt string, brc20repeat string, feePerBytes string) (txid string, txids []string, fee int64, err error) {
+func checkParamsForBRC20(brc20op *string, brc20tick *string, brc20amt *string) bool {
+	if *brc20op == "" {
+		fmt.Println("Error: arg --op is required. ")
+		return true
+	} else if *brc20op != "mint" && *brc20op != "transfer" && *brc20op != "deploy" {
+		fmt.Println("Error: arg --op must be `mint`, `transfer` or `deploy`. ")
+		return true
+	} else if *brc20tick == "" {
+		fmt.Println("Error: arg --tick is required. ")
+		return true
+	} else if *brc20amt == "" {
+		fmt.Println("Error: arg --amt is required. ")
+		return true
+	}
+	return false
+}
+
+func constructBRC20(forEstimate bool, brc20op string, brc20tick string, brc20amt string, brc20repeat int, feePerBytes int) (txid string, txids []string, fee int64, err error) {
 	// ------------------- Log the basic information -------------------
-	fmt.Println("\n[INFO] constructing tx...")
 	fmt.Println("is simulate:", forEstimate)
 
 	// ------------------- Define network parameters -------------------
@@ -67,14 +100,14 @@ func constructBRC20(forEstimate bool, brc20op string, brc20tick string, brc20amt
 	if err != nil {
 		return
 	}
-	fmt.Println("net:", netParams.Name)
+	fmt.Println("bitcoin net:", netParams.Name)
 
 	// ------------------- Load the address -------------------
 	utxoTaprootAddress, err := btcutil.NewAddressTaproot(schnorr.SerializePubKey(txscript.ComputeTaprootKeyNoScript(wifKey.PrivKey.PubKey())), netParams)
 	if err != nil {
 		return
 	}
-	fmt.Println("utxo address:", utxoTaprootAddress.EncodeAddress())
+	fmt.Println("\nyour address:", utxoTaprootAddress.EncodeAddress())
 
 	// --------------------- Collect UTXOs ---------------------
 	unspentList, err := btcApiClient.ListUnspent(utxoTaprootAddress)
@@ -86,6 +119,7 @@ func constructBRC20(forEstimate bool, brc20op string, brc20tick string, brc20amt
 		return
 	}
 	vinAmount := 0
+	available := 0
 	commitTxOutPointList := make([]*wire.OutPoint, 0)
 	commitTxPrivateKeyList := make([]*btcec.PrivateKey, 0)
 	utxoValues := make([]string, len(unspentList))
@@ -95,12 +129,13 @@ func constructBRC20(forEstimate bool, brc20op string, brc20tick string, brc20amt
 			utxoValues[i] = fmt.Sprintf("%s (unusable)", utxoValues[i])
 			continue
 		}
+		available++
 		commitTxOutPointList = append(commitTxOutPointList, unspentList[i].Outpoint)
 		commitTxPrivateKeyList = append(commitTxPrivateKeyList, wifKey.PrivKey)
 		vinAmount += int(unspentList[i].Output.Value)
 	}
-	fmt.Println("utxo count:", len(unspentList))
-	fmt.Println("utxo value list: [", strings.Join(utxoValues, ", "), "]")
+	fmt.Println("your utxo count:", len(unspentList), "(", available, "available)")
+	// fmt.Println("your utxo value list: [", strings.Join(utxoValues, ", "), "]")
 
 	// --------------- Construct the inscription data ---------------
 	dataList := make([]ord.InscriptionData, 0)
@@ -110,40 +145,35 @@ func constructBRC20(forEstimate bool, brc20op string, brc20tick string, brc20amt
 		Body:        []byte(ordinalText),
 		Destination: utxoTaprootAddress.EncodeAddress(),
 	}
-	count, err := strconv.Atoi(brc20repeat)
-	if err != nil {
-		return
-	}
-	for i := 0; i < count; i++ {
+	for i := 0; i < brc20repeat; i++ {
 		dataList = append(dataList, mint)
 	}
-	fmt.Println("inscription text:", ordinalText)
-	fmt.Println("repeat mint times: ", len(dataList))
+	fmt.Println("\nBRC20 inscription text :", ordinalText)
+	fmt.Println("BRC20 repeat mint times: ", len(dataList))
 
 	// --------------- Construct the inscription request ---------------
-	txFee, err := strconv.Atoi(feePerBytes)
 	if err != nil {
 		return
 	}
 	request := ord.InscriptionRequest{
 		CommitTxOutPointList:   commitTxOutPointList,
 		CommitTxPrivateKeyList: commitTxPrivateKeyList,
-		CommitFeeRate:          int64(txFee),
-		FeeRate:                int64(txFee),
+		CommitFeeRate:          int64(feePerBytes),
+		FeeRate:                int64(feePerBytes),
 		DataList:               dataList,
 		SingleRevealTxOnly:     false,
 	}
 
 	// ----------------- Prepare the inscription tool -----------------
-	fmt.Println("current balance:", float32(vinAmount)/1e8)
-	fmt.Print("total spent fee: ")
+	fmt.Println("\nyour current balance:", float32(vinAmount)/1e8)
+	fmt.Print("your total spent fee: ")
 	tool, err := ord.NewInscriptionToolWithBtcApiClient(netParams, btcApiClient, &request)
 	if err != nil {
 		fmt.Println("new tool err: ", err)
 		return
 	}
 	fee = tool.CalculateFee()
-	fmt.Println("balance after inscription:", (float32(vinAmount)-float32(fee))/1e8)
+	fmt.Println("your balance after inscription:", (float32(vinAmount)-float32(fee))/1e8)
 	if forEstimate {
 		return
 	}
